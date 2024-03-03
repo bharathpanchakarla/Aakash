1f12a971df7d5375b14f36864a83743b1087bd49
aakashpanch <161831833+aakashpanch@users.noreply.github.com>
2024-03-03 11:25:15 +0100

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,7 +2,7 @@ metamodel_dict = {
     "nodes": [
         {
             "type": "Person",
-            "edges": ["Friends With", "Parent of", "Child of", "Sibling of", "Colleague of"],
+            "edges": ["Friends With", "Parent of", "Child of", "Sibling of", "Colleague of", "Wife of"],
         },
         {
             "type": "pets",
